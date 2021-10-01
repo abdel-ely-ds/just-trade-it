5cd217ba580dd4562f2137c44d306919bc8080c2
abdel-ely-ds <abdel.ely.ds@gmail.com>
2021-10-01 22:54:09 +0200

    wip

--- tradeit/optimization/datasets_utils/dataset_builder.py
+++ tradeit/optimization/datasets_utils/dataset_builder.py
@@ -25,7 +25,6 @@ class DatasetBuilder:
         self._indicators = indicators
         self._splitter = splitter
         self._features = list(self._indicators.keys())
-        self._n_features = len(self._features)
 
     @property
     def backtest_results(self):
@@ -47,16 +46,6 @@ class DatasetBuilder:
     def features(self):
         return self._features
 
-    def _enrich_stock(self, stock: pd.DataFrame) -> None:
-        for ind_name, ind in self.indicators:
-            stock[ind_name] = ind(stock)
-
-    @staticmethod
-    def _transform_stock(stock):
-        for f in [OPEN, HIGH, LOW, CLOSE]:
-            stock[f] = (stock[f].pct_change(1)).cumsum()
-        stock.dropna(inplace=True)
-
     @lru_cache(maxsize=100)
     def build(
             self,
@@ -71,16 +60,18 @@ class DatasetBuilder:
                 1,
             )
         )
-        for stock_name in tqdm(stock_names):
+        for stock_name in tqdm(stock_names[:1]):
             try:
                 stock = pre_process_stock(
                     pd.read_csv(os.path.join(prefix_path, stock_name))
                 )
-                x_tmp, y_tmp = self._build(stock, stock_name, history=history)
-                x, y = np.append(x, x_tmp, axis=0), np.append(y, y_tmp, axis=0)
+                x_tmp, y_tmp = self._build(stock, stock_name.split(".")[0], history=history)
+                x = np.append(x, x_tmp, axis=0)
+                y = np.append(y, y_tmp, axis=0)
 
-            except:
-                pass
+            except Exception as e:
+                print("error: ", e)
+                print("stock: ", stock_name)
 
         return self.splitter(
             x[1:], y[1:], random_state=random_state, percentage=percentage
@@ -89,24 +80,40 @@ class DatasetBuilder:
     def _build(
             self, stock: pd.DataFrame, symbol: str, history: int = 10
     ) -> Tuple[np.array, np.array]:
-        x = np.array(np.zeros((1, history * len(self.indicators))))
+        x = np.array(np.zeros((1, history * len(self.features))))
         y = np.array(
             np.zeros(
                 1,
             )
         )
-        print(self.backtest_results[SYMBOL])
+        # build a a feature dataframe
+        stock = self._build_features(stock)
+
         for ind, trade in self.backtest_results[
             self.backtest_results[SYMBOL] == symbol
         ].iterrows():
             entry_bar = int(trade[ENTRY_BAR])
             x = np.append(
                 x,
-                stock.iloc[entry_bar - self._n_features: entry_bar][self.features]
+                stock.iloc[entry_bar - history: entry_bar]
                     .to_numpy()
-                    .reshape((1, self._n_features * len(self.features))),
+                    .reshape((1, history * len(self.features))),
                 axis=0,
             )
             y = np.append(y, trade[IS_LOSING])
 
         return x[1:], y[1:]
+
+    def _build_features(self, stock: pd.DataFrame) -> pd.DataFrame:
+        transformed_stock = self._transform_stock(stock)
+        for ind_name, ind in self.indicators.items():
+            transformed_stock[ind_name] = ind(stock)
+        return transformed_stock[self.features]
+
+    @staticmethod
+    def _transform_stock(stock):
+        stock_copy = stock.copy()
+        for f in [OPEN, HIGH, LOW, CLOSE]:
+            stock_copy[f] = (stock_copy[f].pct_change(1)).cumsum()
+        stock_copy.dropna(inplace=True)
+        return stock_copy
